e89c387c940e23560ad6667546f3841e5bc5cb7c
Juan Cruz Viotti <jv@jviotti.com>
2024-08-27 16:42:00 -0400

    Interpret instances as JSONL in JSON Toolkit (#2)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- implementations/jsontoolkit/main.cc
+++ implementations/jsontoolkit/main.cc
@@ -1,9 +1,11 @@
 #include <sourcemeta/jsontoolkit/json.h>
+#include <sourcemeta/jsontoolkit/jsonl.h>
 #include <sourcemeta/jsontoolkit/jsonschema.h>
 
 #include <chrono>
 #include <iostream>
 #include <filesystem>
+#include <vector>
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -13,7 +15,11 @@ int main(int argc, char **argv) {
 
   const std::filesystem::path example{argv[1]};
   const auto schema{sourcemeta::jsontoolkit::from_file(example / "schema.json")};
-  const auto instance{sourcemeta::jsontoolkit::from_file(example / "instances.jsonl")};
+  auto stream{sourcemeta::jsontoolkit::read_file(example / "instances.jsonl")};
+  std::vector<sourcemeta::jsontoolkit::JSON> instances;
+  for (const auto &instance : sourcemeta::jsontoolkit::JSONL{stream}) {
+    instances.push_back(instance);
+  }
 
   const auto schema_template{sourcemeta::jsontoolkit::compile(
       schema, sourcemeta::jsontoolkit::default_schema_walker,
@@ -21,12 +27,16 @@ int main(int argc, char **argv) {
       sourcemeta::jsontoolkit::default_schema_compiler)};
 
   const auto timestamp_start{std::chrono::high_resolution_clock::now()};
-  const auto result{sourcemeta::jsontoolkit::evaluate(
-    schema_template, instance)};
-  const auto timestamp_end{std::chrono::high_resolution_clock::now()};
-  if (!result) {
-    return EXIT_FAILURE;
+
+  for (const auto &instance : instances) {
+    const auto result{sourcemeta::jsontoolkit::evaluate(
+      schema_template, instance)};
+    if (!result) {
+      return EXIT_FAILURE;
+    }
   }
+
+  const auto timestamp_end{std::chrono::high_resolution_clock::now()};
 
   const auto duration{
     std::chrono::duration_cast<std::chrono::nanoseconds>(timestamp_end - timestamp_start)};
