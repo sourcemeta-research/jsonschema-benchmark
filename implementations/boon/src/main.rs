0705963fea17875d7399c8fed2d4bd7640474792
Tony Gorez <gorez.tony@gmail.com>
2024-08-28 19:55:20 +0200

    benchmark: add boon (#11)
    Fix #5
    
    ---------
    
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ implementations/boon/src/main.rs
@@ -0,0 +1,42 @@
+use std::{error::Error, fs::File, io::{BufReader, BufRead}, time::Instant};
+use boon::{Compiler, Schemas};
+use serde_json::Value;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+  // Get arguments
+  let args: Vec<String> = env::args().collect();
+  let example_folder = &args[1];
+
+  // Get the schema and instance paths
+  let schema_file =   std::fs::canonicalize(example_folder.to_owned() + "/schema.json")?;
+  let instance_file = std::fs::canonicalize(example_folder.to_owned() + "/instances.jsonl")?;
+
+  // Read the instance file
+  let file = File::open(&instance_file)?;
+  let reader = BufReader::new(file);
+
+  // Compile the schema
+  let mut schemas = Schemas::new();
+  let mut compiler = Compiler::new();
+  let sch_index = compiler.compile(schema_file.to_str().ok_or("NULL")?, &mut schemas)?;
+
+  // Serialize instance lines
+  let mut serde_lines = std::vec::Vec::new();
+  for line in reader.lines() {
+      let line = line?;
+      let instance: Value = serde_json::from_str(&line)?;
+      serde_lines.push(instance);
+  }
+
+  // Validate the instances
+  let start = Instant::now();
+  for line in serde_lines {
+    let result = schemas.validate(&line, sch_index);
+    assert!(result.is_ok(), "Validation failed for line: {}", line);
+  }
+  let duration = start.elapsed().as_nanos();
+  println!("{:?}", duration);
+
+  Ok(())
+}
