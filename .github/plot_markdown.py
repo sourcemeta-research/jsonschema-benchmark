fbaa073293a7d5c5b2170644e6a76cd40a04074b
Michael Mior <mmior@mail.rit.edu>
2024-08-28 13:50:55 -0400

    Generate plots of performance results

---
+++ .github/plot_markdown.py
@@ -0,0 +1,12 @@
+import glob
+import json
+import os
+
+
+if __name__ == '__main__':
+    img_urls = json.loads(os.environ['IMG_URLS'])
+    img_files = glob.glob('dist/results/plots/*.png')
+    for (url, file) in zip(img_urls, img_files):
+        name = file.split('/')[-1].split('.')[0]
+        print(f"## {name}")
+        print(f"![{name}]({url})\n")
