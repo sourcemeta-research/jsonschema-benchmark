a9890070410cdfb8176368a59573d1142940d8fd
Tony Gorez <gorez.tony@gmail.com>
2024-08-29 22:40:54 +0200

    refactor: create a func
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- implementations/jsontoolkit/main.cc
+++ implementations/jsontoolkit/main.cc
@@ -7,6 +7,39 @@
 #include <iostream>
 #include <vector>
 
+int validate(const std::filesystem::path &example) {
+  const auto schema{
+      sourcemeta::jsontoolkit::from_file(example / "schema.json")};
+  auto stream{sourcemeta::jsontoolkit::read_file(example / "instances.jsonl")};
+  std::vector<sourcemeta::jsontoolkit::JSON> instances;
+  for (const auto &instance : sourcemeta::jsontoolkit::JSONL{stream}) {
+    instances.push_back(instance);
+  }
+
+  const auto schema_template{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const auto timestamp_start{std::chrono::high_resolution_clock::now()};
+
+  for (const auto &instance : instances) {
+    const auto result{
+        sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    if (!result) {
+      return EXIT_FAILURE;
+    }
+  }
+
+  const auto timestamp_end{std::chrono::high_resolution_clock::now()};
+
+  const auto duration{std::chrono::duration_cast<std::chrono::nanoseconds>(
+      timestamp_end - timestamp_start)};
+  std::cout << duration.count() << "\n";
+
+  return EXIT_SUCCESS;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <schema>\n";
@@ -15,37 +48,7 @@ int main(int argc, char **argv) {
 
   try {
     const std::filesystem::path example{argv[1]};
-    const auto schema{
-        sourcemeta::jsontoolkit::from_file(example / "schema.json")};
-    auto stream{
-        sourcemeta::jsontoolkit::read_file(example / "instances.jsonl")};
-    std::vector<sourcemeta::jsontoolkit::JSON> instances;
-    for (const auto &instance : sourcemeta::jsontoolkit::JSONL{stream}) {
-      instances.push_back(instance);
-    }
-
-    const auto schema_template{sourcemeta::jsontoolkit::compile(
-        schema, sourcemeta::jsontoolkit::default_schema_walker,
-        sourcemeta::jsontoolkit::official_resolver,
-        sourcemeta::jsontoolkit::default_schema_compiler)};
-
-    const auto timestamp_start{std::chrono::high_resolution_clock::now()};
-
-    for (const auto &instance : instances) {
-      const auto result{
-          sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
-      if (!result) {
-        return EXIT_FAILURE;
-      }
-    }
-
-    const auto timestamp_end{std::chrono::high_resolution_clock::now()};
-
-    const auto duration{std::chrono::duration_cast<std::chrono::nanoseconds>(
-        timestamp_end - timestamp_start)};
-    std::cout << duration.count() << "\n";
-
-    return EXIT_SUCCESS;
+    return validate(example);
   } catch (const std::exception &e) {
     std::cerr << "Error during jsontoolkit benchmark: " << e.what() << "\n";
     return EXIT_FAILURE;
