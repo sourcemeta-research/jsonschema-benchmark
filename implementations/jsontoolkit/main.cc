1335db5d4e0516ac37b258cd61d98a5f9837becc
Tony Gorez <gorez.tony@gmail.com>
2024-08-30 17:38:51 +0200

    fix: wrap cpp code in a try/catch (#23)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- implementations/jsontoolkit/main.cc
+++ implementations/jsontoolkit/main.cc
@@ -3,18 +3,13 @@
 #include <sourcemeta/jsontoolkit/jsonschema.h>
 
 #include <chrono>
-#include <iostream>
 #include <filesystem>
+#include <iostream>
 #include <vector>
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <schema>\n";
-    return EXIT_FAILURE;
-  }
-
-  const std::filesystem::path example{argv[1]};
-  const auto schema{sourcemeta::jsontoolkit::from_file(example / "schema.json")};
+int validate(const std::filesystem::path &example) {
+  const auto schema{
+      sourcemeta::jsontoolkit::from_file(example / "schema.json")};
   auto stream{sourcemeta::jsontoolkit::read_file(example / "instances.jsonl")};
   std::vector<sourcemeta::jsontoolkit::JSON> instances;
   for (const auto &instance : sourcemeta::jsontoolkit::JSONL{stream}) {
@@ -30,8 +25,8 @@ int main(int argc, char **argv) {
   const auto timestamp_start{std::chrono::high_resolution_clock::now()};
 
   for (const auto &instance : instances) {
-    const auto result{sourcemeta::jsontoolkit::evaluate(
-      schema_template, instance)};
+    const auto result{
+        sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
     if (!result) {
       return EXIT_FAILURE;
     }
@@ -39,9 +34,24 @@ int main(int argc, char **argv) {
 
   const auto timestamp_end{std::chrono::high_resolution_clock::now()};
 
-  const auto duration{
-    std::chrono::duration_cast<std::chrono::nanoseconds>(timestamp_end - timestamp_start)};
+  const auto duration{std::chrono::duration_cast<std::chrono::nanoseconds>(
+      timestamp_end - timestamp_start)};
   std::cout << duration.count() << "\n";
 
   return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <schema>\n";
+    return EXIT_FAILURE;
+  }
+
+  try {
+    const std::filesystem::path example{argv[1]};
+    return validate(example);
+  } catch (const std::exception &e) {
+    std::cerr << "Error during jsontoolkit benchmark: " << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
 }
