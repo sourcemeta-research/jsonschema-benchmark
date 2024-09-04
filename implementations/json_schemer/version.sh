92d74a55399386dac1689d186e3dfaf17fe8a3b4
Michael Mior <mmior@mail.rit.edu>
2024-09-04 08:14:25 -0400

    Use Perl for Ruby version script

--- implementations/json_schemer/version.sh
+++ implementations/json_schemer/version.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 
 # Extract the version of the package (json_schemer) from Gemfile.lock
-json_schemer_version=$(grep -Po '(?<=json_schemer \().*(?=\))' implementations/json_schemer/Gemfile.lock)
+json_schemer_version=$(perl -nle'print $& while m{(?<=json_schemer \().*(?=\))}g' implementations/json_schemer/Gemfile.lock)
 
 # Output the version
 echo "$json_schemer_version"
