a30f26caf099c6868011a7621aec3f085c185024
Michael Mior <mmior@mail.rit.edu>
2024-10-01 15:28:49 -0400

    Switch to draft 07 (#50)

--- implementations/ajv/main.mjs
+++ implementations/ajv/main.mjs
@@ -1,5 +1,5 @@
 import Ajv from 'ajv';
-import draft4schema from 'ajv/lib/refs/json-schema-draft-04.json' with { type: 'json' };
+import draft7schema from 'ajv/lib/refs/json-schema-draft-07.json' with { type: 'json' };
 import fs from 'fs';
 import readline from 'readline';
 import { performance } from 'perf_hooks';
@@ -31,7 +31,7 @@ async function validateSchema(schemaPath, instancePath) {
     validateSchema: false
   });
 
-  ajv.addMetaSchema(draft4schema);
+  ajv.addMetaSchema(draft7schema);
   const validate = ajv.compile(schema);
 
   const instances = [];
