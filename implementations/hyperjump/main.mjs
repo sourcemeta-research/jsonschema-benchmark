097701afbc7988897d171d368e1c543451a6b0b7
Michael Mior <mmior@mail.rit.edu>
2024-10-01 20:17:05 -0400

    Add hyperjump implementation (#52)

---
+++ implementations/hyperjump/main.mjs
@@ -0,0 +1,61 @@
+import { registerSchema, validate } from "@hyperjump/json-schema/draft-07";
+import fs from 'fs';
+import readline from 'readline';
+import { performance } from 'perf_hooks';
+
+function readJSONFile(filePath) {
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(fileContent);
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+async function* readJSONLines(filePath) {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(filePath),
+  });
+  for await (const line of rl) {
+    yield JSON.parse(line);
+  }
+}
+
+async function validateSchema(schemaPath, instancePath) {
+  const schema = readJSONFile(schemaPath);
+
+  const schemaId = schema["$id"] || "https://example.com" + schemaPath;
+  registerSchema(schema, schemaId);
+
+  const instances = [];
+  for await (const instance of readJSONLines(instancePath)) {
+    instances.push(instance);
+  }
+  let failed = false;
+  const startTime = performance.now();
+  for (const instance of instances) {
+    const output = await validate(schemaId, instance);
+    if (!output.valid) {
+      failed = true;
+    }
+  }
+
+  const endTime = performance.now();
+
+  const durationNs = (endTime - startTime) * 1e6;
+  console.log(durationNs.toFixed(0));
+
+  // Exit with non-zero status on validation failure
+  if (failed) {
+    process.exit(1);
+  }
+}
+
+if (process.argv.length !== 4) {
+  process.exit(1);
+}
+
+const schemaPath = process.argv[2];
+const instancePath = process.argv[3];
+
+await validateSchema(schemaPath, instancePath);
