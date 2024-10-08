d69f355aea7a94caee32f13855e855a9dd39d827
Michael Mior <mmior@mail.rit.edu>
2024-10-08 10:55:35 -0400

    Mark implementations that are winning by far

--- .github/csv_min.py
+++ .github/csv_min.py
@@ -6,17 +6,36 @@ reader = csv.DictReader(open('dist/report.csv'))
 min_val = defaultdict(lambda: sys.maxsize)
 min_impl = {}
 for row in reader:
-    if int(row['nanoseconds']) < min_val[row['name']] \
-            and row['exit_status'] == '0':
+    # Skip failed runs
+    if row['exit_status'] != '0':
+        continue
+
+    if int(row['nanoseconds']) < min_val[row['name']]:
         min_val[row['name']] = int(row['nanoseconds'])
         min_impl[row['name']] = row['implementation']
+
+reader = csv.DictReader(open('dist/report.csv'))
+next_min_val = defaultdict(lambda: sys.maxsize)
+for row in reader:
+    # Skip failed runs
+    if row['exit_status'] != '0':
+        continue
+
+    if int(row['nanoseconds']) > min_val[row['name']] \
+            and int(row['nanoseconds']) < next_min_val[row['name']]:
+        next_min_val[row['name']] = int(row['nanoseconds'])
 
 reader = csv.DictReader(open('dist/report.csv'))
 writer = csv.DictWriter(sys.stdout, fieldnames=row.keys())
 writer.writeheader()
 for row in reader:
+    row_emoji = ''
     if min_impl[row['name']] == row['implementation']:
-        row['name'] += ' :white_check_mark:'
+        row_emoji += ' :white_check_mark:'
+        if min_val[row['name']] <= next_min_val[row['name']] * 0.8:
+            row_emoji += ' :trophy:'
     if row['exit_status'] != '0':
-        row['name'] += ' :x:'
+        row_emoji += ' :x:'
+
+    row['name'] += row_emoji
     writer.writerow(row)
