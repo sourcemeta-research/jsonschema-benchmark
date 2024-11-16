7d9ad813216a1f71e36c42bcd3bc6a5d216db410
Michael Mior <mmior@mail.rit.edu>
2024-11-16 15:57:36 -0500

    Label cold, warm, and compile times separate

--- .github/csv_min.py
+++ .github/csv_min.py
@@ -27,34 +27,61 @@ data.reset_index(inplace=True)
 data.set_index(["implementation", "name"], inplace=True)
 
 # Get the fastest implementations
-min_impls = set()
+min_cold_impls = set()
 for schema, impl in data[data["exit_status"] == 0].groupby("name")["cold_ns"].idxmin():
-    min_impls.add((schema, impl))
+    min_cold_impls.add((schema, impl))
+min_warm_impls = set()
+for schema, impl in data[data["exit_status"] == 0].groupby("name")["warm_ns"].idxmin():
+    min_warm_impls.add((schema, impl))
+min_compile_impls = set()
+for schema, impl in data[data["exit_status"] == 0].groupby("name")["compile_ns"].idxmin():
+    min_compile_impls.add((schema, impl))
 
 # Get the next fastest implementation
-next_fastest = (
+next_fastest_cold = (
     data[data["exit_status"] == 0].groupby("name").agg({"cold_ns": get_second})
+)
+next_fastest_warm = (
+    data[data["exit_status"] == 0].groupby("name").agg({"warm_ns": get_second})
+)
+next_fastest_compile = (
+    data[data["exit_status"] == 0].groupby("name").agg({"compile_ns": get_second})
 )
 
 # Label each implementation which was the fastest
 new_index = data.index.to_list()
 for i, (impl, schema) in enumerate(new_index):
-    if (impl, schema) in min_impls:
-        new_name = impl + ":white_check_mark:"
+    if (impl, schema) in min_cold_impls:
+        suffix = ":white_check_mark:"
         fast_time = data.at[(impl, schema), "cold_ns"]
-        next_time = next_fastest.loc[schema]["cold_ns"]
+        next_time = next_fastest_cold.loc[schema]["cold_ns"]
 
         # If this implementation is 20% faster than the next, add a trophy
         if fast_time < next_time * 0.8:
-            new_name += ":trophy:"
+            suffix += " :trophy:"
 
-        new_index[i] = (new_name, impl)
+        data.at[(impl, schema), "cold_ns"] = f"{fast_time} {suffix}"
 
-# Apply the new names
-data.index = pd.MultiIndex.from_tuples(new_index, names=["implementation", "schema"])
+    if (impl, schema) in min_warm_impls:
+        suffix = ":white_check_mark:"
+        fast_time = data.at[(impl, schema), "warm_ns"]
+        next_time = next_fastest_warm.loc[schema]["warm_ns"]
 
-# Reset column order
-data.reset_index(inplace=True)
-data.set_index(["implementation", "version", "schema"], inplace=True)
+        # If this implementation is 20% faster than the next, add a trophy
+        if fast_time < next_time * 0.8:
+            suffix += " :trophy:"
+
+        data.at[(impl, schema), "warm_ns"] = f"{fast_time} {suffix}"
+
+    if (impl, schema) in min_compile_impls:
+        suffix = ":white_check_mark:"
+        fast_time = data.at[(impl, schema), "compile_ns"]
+        next_time = next_fastest_compile.loc[schema]["compile_ns"]
+
+        # If this implementation is 20% faster than the next, add a trophy
+        if fast_time < next_time * 0.8:
+            suffix += " :trophy:"
+
+        data.at[(impl, schema), "compile_ns"] = f"{fast_time} {suffix}"
 
 data.to_csv(sys.stdout)
