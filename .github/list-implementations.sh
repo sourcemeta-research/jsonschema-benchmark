48f717d016b6bbe2dff299940f687211e75dc08c
Michael Mior <mmior@mail.rit.edu>
2025-01-14 15:09:50 -0500

    Skip cache for implementations modified by PRs

--- .github/list-implementations.sh
+++ .github/list-implementations.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 # Get a list of all implementations (with some potentially ignored)
-all_impls=$(make NO_IGNORE=$NO_IGNORE list)
+all_impls=$(make NO_IGNORE=$NO_IGNORE list | sort)
 
 # Add implementations changed in the PR
 if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
   git fetch origin $GITHUB_BASE_REF --depth 1
-  pr_impls=$(git diff --name-only FETCH_HEAD..HEAD | grep '^implementations/' | cut -d/ -f2 | uniq)
+  pr_impls=$(git diff --name-only FETCH_HEAD..HEAD | grep '^implementations/' | cut -d/ -f2 | sort -u)
 fi
 
+# Get all implementations that are modified by the PR
+base_impls=$(comm -23 <(echo "$all_impls") <(echo "$pr_impls"))
+
 # Format as JSON like {"include": [{"impl" "foo"}, ...]}
-echo -e "$all_impls\n$pr_impls" | grep -v '^$' | sed 's/ *$//' | sort -u | tr '\n' ' ' | sed 's/ $//;s/^/{"include":\[{"impl":"/;s/ /"}, {"impl":"/g;s/$/"}]}/'
+echo -n '{"include":['
+(echo "$base_impls" | while read impl; do
+  echo -n '{"impl": "'$impl'", "skip_cache": false},'
+done
+echo "$pr_impls" | while read impl; do
+  [ -z "$impl" ] || echo -n '{"impl": "'$impl'", "skip_cache": true},'
+done) | sed 's/,$//' # Remove trailing comma
+echo "]}"
