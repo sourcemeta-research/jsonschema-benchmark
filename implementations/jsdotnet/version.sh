823d20213099938196d995d76f398bf3dee48e66
Michael Mior <mmior@mail.rit.edu>
2025-02-07 17:16:55 -0500

    Fix versions scripts after implementation rename (#139)

--- implementations/jsdotnet/version.sh
+++ implementations/jsdotnet/version.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 
-grep 'Include="JsonSchema.Net"' implementations/jsonschemadotnet/bench.csproj | tr ' ' '\n' | grep Version | cut -d= -f2 | tr -d '"'
+grep 'Include="JsonSchema.Net"' implementations/jsdotnet/bench.csproj | tr ' ' '\n' | grep Version | cut -d= -f2 | tr -d '"'
--- implementations/kmp/version.sh
+++ implementations/kmp/version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -E 'validator = ".*"' implementations/kmp-json-schema-validator/gradle/libs.versions.toml | cut -d= -f2 | tr -d '" '
+grep -E 'validator = ".*"' implementations/kmp/gradle/libs.versions.toml | cut -d= -f2 | tr -d '" '
