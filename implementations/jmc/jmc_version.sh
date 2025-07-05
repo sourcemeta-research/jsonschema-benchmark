b139de66084d16fd4ca5ffc89307bb4ce41f46f9
Fabien Coelho <fabien@coelho.net>
2025-07-05 21:25:15 +0200

    fix version script to work out of the container

---
+++ implementations/jmc/jmc_version.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+function git_version()
+{
+  local dir=$1
+  pushd $dir > /dev/null
+  git log -1 | head -1 | cut -d' ' -f2 | cut -c -8
+}
+
+jmc_ver=$(/venv/bin/jmc --version)
+jmc_git=$(git_version ./json-model)
+jsu_ver=$(/venv/bin/jsu-model --version)
+jsu_git=$(git_version ./json-schema-utils)
+
+echo "jmc=$jmc_ver-$jmc_git jsu=$jsu_ver-$jsu_git"
