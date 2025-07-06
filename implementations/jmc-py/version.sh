d4d001dbb030b2277db90a6b229f4210bfc1aed1
Fabien Coelho <fabien@coelho.net>
2025-07-06 09:09:40 +0200

    show runtime version as well

--- implementations/jmc-py/version.sh
+++ implementations/jmc-py/version.sh
@@ -1,1 +1,2 @@
-../jmc/version.sh
+#! /bin/sh
+docker run --rm --entrypoint /app/jmc_version.sh jsonschema-benchmark/jmc:latest Python
--- implementations/jmc/jmc_version.sh
+++ implementations/jmc/jmc_version.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
+case $1 in
+    C|c)
+        runtime="cc=$(cc --version | head -1 | cut -d' ' -f4)"
+        ;;
+    JS|js|JavaScript|javascript)
+        runtime="node=$(node --version)"
+        ;;
+    PY|py|Python|python)
+        runtime=$(python --version | tr ' ' '=')
+        ;;
+    *)
+        runtime="unknown"
+esac
+
 function git_version()
 {
-  local dir=$1
-  pushd $dir > /dev/null
-  git log -1 | head -1 | cut -d' ' -f2 | cut -c -8
+    local dir=$1
+    pushd $dir > /dev/null
+    git log -1 | head -1 | cut -d' ' -f2 | cut -c -8
 }
 
 jmc_ver=$(/venv/bin/jmc --version)
@@ -12,4 +26,4 @@ jmc_git=$(git_version ./json-model)
 jsu_ver=$(/venv/bin/jsu-model --version)
 jsu_git=$(git_version ./json-schema-utils)
 
-echo "jmc=$jmc_ver-$jmc_git jsu=$jsu_ver-$jsu_git"
+echo "$runtime jmc=$jmc_ver-$jmc_git jsu=$jsu_ver-$jsu_git"
