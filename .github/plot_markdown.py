9ab2f74ded9dda3c7be4bad8ea1eb70aaacad0a5
Michael Mior <mmior@mail.rit.edu>
2025-09-24 13:25:21 -0400

    Use data URIs for GHA summary images

--- .github/plot_markdown.py
+++ .github/plot_markdown.py
@@ -1,3 +1,4 @@
+import base64
 import glob
 import json
 import os
@@ -5,22 +6,21 @@ import sys
 
 
 if __name__ == '__main__':
-    img_urls = json.loads(os.environ['IMG_URLS'])
-
     # Allow specifying a folder or a single file
     if os.path.isdir(sys.argv[1]):
         img_files = sorted(glob.glob(os.path.join(sys.argv[1], '*.png')))
     else:
         img_files = [sys.argv[1]]
 
-    # Make sure we have the correct number of images
-    assert(len(img_urls) == len(img_files))
-
-    for (url, file) in zip(img_urls, img_files):
+    for file in img_files:
         if len(sys.argv) > 2:
             assert(os.path.isfile(sys.argv[1]))
             name = sys.argv[2]
         else:
             name = file.split('/')[-1].split('.')[0]
+
+        # Convert the image into a data URL
+        data_url = "data:image/png;base64," + base64.b64encode(open(file, "rb").read()).decode()
+
         print(f"## {name}")
-        print(f"![{name}]({url})\n")
+        print(f"![{name}]({data_url})\n")
