36f7135874130af1ce73e1fe3a0ef3cc7570d87b
Fabien Coelho <fabien@coelho.net>
2026-02-05 14:58:17 +0100

    Add JSU Compiler with JMC Backend for C, Python, JS and Perl.

---
+++ implementations/jsu-c/jsu_version.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+case $1 in
+    C|c)
+        runtime="cc=$(cc --version | head -1 | cut -d' ' -f4)"
+        ;;
+    JS|js|JavaScript|javascript)
+        runtime="node=$(node --version)"
+        ;;
+    PY|py|Python|python)
+        runtime=$(python --version | tr ' ' '=')
+        ;;
+    PL|pl|Perl|perl)
+        runtime="perl=$(perl -e 'use English; print $PERL_VERSION';)"
+        ;;
+    *)
+        runtime="unknown"
+esac
+
+function git_version()
+{
+    git -C $1 rev-parse --short=8 HEAD
+}
+
+jsu_ver=$(/venv/bin/jsu-compile --version)
+jsu_git=$(git_version ./json-schema-utils)
+
+echo "$runtime jsu=$jsu_ver-$jsu_git"
